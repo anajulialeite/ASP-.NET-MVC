57e6054c29d1819ce3d55763e0b0cd3a490cd5bb
Ana Júlia Leite <leite.anajulia25@gmail.com>
2025-04-20 00:16:25 -0300

    adicionando projeto

---
+++ Models/tableAttribute.cs
@@ -0,0 +1,12 @@
+﻿namespace LanchesMac.Models
+{
+    public class tableAttribute : Attribute
+    {
+        private string v;
+
+        public tableAttribute(string v)
+        {
+            this.v = v;
+        }
+    }
+}
